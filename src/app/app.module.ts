8a36e3048548c66cbb0c837f66ecb8a1f1a44acb
ekhaion <ekhaion@hotmail.com>
2021-08-02 12:41:32 +0200

    Components à fond

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FourOhFourComponent,
+    HomePageComponent,
+    HeaderComponent,
+    CardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
